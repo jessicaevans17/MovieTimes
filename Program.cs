a83eb2e1e675802aa1b72af2261ae1a1612d507f
Jessica Evans <jme@mail.usf.edu>
2019-09-16 10:59:52 -0500

    project created

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MovieTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
